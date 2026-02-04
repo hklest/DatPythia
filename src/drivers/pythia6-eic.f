e1440742593269688431e4804be920bc0214bb83
hklest <42772735+hklest@users.noreply.github.com>
2026-02-04 17:03:41 -0600

    Align PYTHIA output with SIMC selection

--- src/drivers/pythia6-eic.f
+++ src/drivers/pythia6-eic.f
@@ -23,6 +23,7 @@ C...Switches for nuclear correction
       DOUBLE PRECISION sqrts, radgamE, radgamp, radgamEnucl
       DOUBLE PRECISION pbeamE, pbeta, pgamma, ebeamE, epznucl
       CHARACTER PARAM*100
+      CHARACTER PARAM2*100
       LOGICAL UseLut, GenLut
 
 c ---------------------------------------------------------------------
@@ -32,9 +33,12 @@ c ---------------------------------------------------------------------
 c---------------------------------------------------------------------
 c     ASCII output file
 c ---------------------------------------------------------------------
-      integer asciiLun
-      parameter (asciiLun=29)
-      CHARACTER*256 outputfilename
+      integer simcLun, pythiaLun
+      parameter (simcLun=29)
+      parameter (pythiaLun=30)
+      CHARACTER*256 simcfilename
+      CHARACTER*256 pythiafilename
+      CHARACTER*256 baseout
       CHARACTER*256 outname
       real*8 deg2rad, maxOff
 
@@ -47,9 +51,18 @@ c---------------------------------------------------------------------
       REAL pi2
       parameter(pi2=3.14159)
 
-      LOGICAL do_simc_out,do_proton,do_pion,do_kaon
+      LOGICAL do_simc_out, do_pythia_event_record
+      LOGICAL do_proton,do_pion,do_kaon
       logical foundE,foundP,hasPhi
       integer je,jp
+      integer keep_pdg, keep_pdgabs_flag, keep_final_flag
+      integer excl_flag, excl_rad_flag, ist
+      logical do_keep_pdg, keep_abs_pdg, keep_final_only, has_keep_pdg
+      logical do_exclusive, excl_allow_rad
+      logical is_exclusive_parent
+      logical simc_event_ok
+      real*8 excl_mass_tol, excl_mass_flag
+      COMMON /EXCLCUTS/ excl_mass_tol
       real*8 pxE,pyE,pzE,eE,pxP,pyP,pzP,eP,phiE,phiP,delPhi,bestPhi
       real*8 vxloc
 
@@ -58,7 +71,8 @@ c---------------------------------------------------------------------
 C       external pydata
 c ---------------------------------------------------------------------
 *  ---- user switches --------------------------------------------------
-      do_simc_out = .true.   ! leave .true. (only this branch extended)
+      do_simc_out = .false.
+      do_pythia_event_record = .true.
       do_proton    = .true.  ! default behaviour
       do_pion      = .false.
       do_kaon      = .false.
@@ -70,11 +84,18 @@ c ---------------------------------------------------------------------
        ltype=11
        masse=PYMASS(11)
        massp=PYMASS(2212)
-       ievent=0
-       genevent=0
-       lastgenevent=0
-       tracknr=0
-       RadState=0 ! Initial or Final State Radiation variable (use with RADGEN - 0 --> No Radiation)
+      ievent=0
+      genevent=0
+      lastgenevent=0
+      tracknr=0
+      RadState=0 ! Initial or Final State Radiation variable (use with RADGEN - 0 --> No Radiation)
+      keep_pdg        = 0
+      do_keep_pdg     = .false.
+      keep_abs_pdg    = .true.
+      keep_final_only = .false.
+      do_exclusive    = .false.
+      excl_allow_rad  = .false.
+      excl_mass_tol   = 0.05d0
 
 C...Read output file name
        READ(*,*) outname
@@ -104,6 +125,73 @@ C...Read nuclear pdf correction order
        READ(*,*) ORDER
 C...Read information for cross section used in radgen
   100  READ(*,'(A)',END=200) PARAM
+       PARAM2 = ADJUSTL(PARAM)
+
+C--- Custom cards (do NOT forward to PYGIVE) ---------------------------
+C    Syntax (space-separated; optional '=' allowed):
+C      KEEP_PDG        223        (0 disables)
+C      KEEP_PDGABS     1          (1=abs match, 0=exact sign)
+C      KEEP_PDG_FINAL  1          (1=final-state only, i.e. K(I,1)=1)
+C      KEEP_EXCLUSIVE  1          (require exclusive ep -> ep + KEEP_PDG)
+C      EXCL_ALLOW_RAD  1          (allow radiative events if RadState!=0)
+C      EXCL_MASS_TOL   0.05       (GeV, hadronic system mass tolerance)
+C      DO_SIMC         1          (1=write SIMC .dat output)
+C      DO_PYTHIA_EVENT_RECORD 1   (1=write PYTHIA event record .txt)
+C-----------------------------------------------------------------------
+
+       if (PARAM2(1:8) .eq. 'KEEP_PDG') then
+          ist = 9
+          if (PARAM2(9:9) .eq. '=') ist = 10
+          read(PARAM2(ist:),*) keep_pdg
+          do_keep_pdg = (keep_pdg .ne. 0)
+          goto 100
+       else if (PARAM2(1:11) .eq. 'KEEP_PDGABS') then
+          ist = 12
+          if (PARAM2(12:12) .eq. '=') ist = 13
+          read(PARAM2(ist:),*) keep_pdgabs_flag
+          keep_abs_pdg = (keep_pdgabs_flag .ne. 0)
+          goto 100
+       else if (PARAM2(1:14) .eq. 'KEEP_PDG_FINAL') then
+          ist = 15
+          if (PARAM2(15:15) .eq. '=') ist = 16
+          read(PARAM2(ist:),*) keep_final_flag
+          keep_final_only = (keep_final_flag .ne. 0)
+          goto 100
+       else if (PARAM2(1:14) .eq. 'KEEP_EXCLUSIVE') then
+          ist = 15
+          if (PARAM2(15:15) .eq. '=') ist = 16
+          read(PARAM2(ist:),*) excl_flag
+          do_exclusive = (excl_flag .ne. 0)
+          goto 100
+       else if (PARAM2(1:13) .eq. 'EXCL_ALLOW_RAD') then
+          ist = 14
+          if (PARAM2(14:14) .eq. '=') ist = 15
+          read(PARAM2(ist:),*) excl_rad_flag
+          excl_allow_rad = (excl_rad_flag .ne. 0)
+          goto 100
+       else if (PARAM2(1:13) .eq. 'EXCL_MASS_TOL') then
+          ist = 14
+          if (PARAM2(14:14) .eq. '=') ist = 15
+          read(PARAM2(ist:),*) excl_mass_flag
+          excl_mass_tol = excl_mass_flag
+          goto 100
+       else if (PARAM2(1:7) .eq. 'DO_SIMC' .or.
+     &          PARAM2(1:7) .eq. 'do_simc') then
+          ist = 8
+          if (PARAM2(8:8) .eq. '=') ist = 9
+          read(PARAM2(ist:),*) keep_final_flag
+          do_simc_out = (keep_final_flag .ne. 0)
+          goto 100
+       else if (PARAM2(1:22) .eq. 'DO_PYTHIA_EVENT_RECORD' .or.
+     &          PARAM2(1:22) .eq. 'do_pythia_event_record') then
+          ist = 23
+          if (PARAM2(23:23) .eq. '=') ist = 24
+          read(PARAM2(ist:),*) keep_final_flag
+          do_pythia_event_record = (keep_final_flag .ne. 0)
+          goto 100
+       end if
+
+C--- Normal PYTHIA cards ------------------------------------------------
        CALL PYGIVE(PARAM)
        GOTO 100
 c ---------------------------------------------------------------------
@@ -222,32 +310,56 @@ C      win=ebeam
 C      call pyinit('fixt','gamma/e-','p+', WIN)
 
 c ---------------------------------------------------------------------
-c     Open ascii output file
+c     Open ascii output file(s)
 c ---------------------------------------------------------------------
-       outputfilename=outname
-       open(asciiLun, file=outputfilename)
-       write(*,*) 'the outputfile will be named: ', outname
+       baseout = outname
+       if (len_trim(outname) .ge. 4) then
+          if (outname(len_trim(outname)-3:len_trim(outname)) .eq.
+     &        '.dat') then
+             baseout = outname(1:len_trim(outname)-4)
+          else if (outname(len_trim(outname)-3:len_trim(outname)) .eq.
+     &        '.txt') then
+             baseout = outname(1:len_trim(outname)-4)
+          end if
+       end if
+
+       if (.not. do_simc_out .and. .not. do_pythia_event_record) then
+          write(*,*) 'WARNING: do_simc=0 and do_pythia_event_record=0.'
+          write(*,*) 'WARNING: no output file will be written.'
+       end if
+
+       if (do_simc_out) then
+          simcfilename = trim(baseout)//'.dat'
+          open(simcLun, file=simcfilename)
+          write(*,*) 'SIMC output file: ', trim(simcfilename)
+       end if
+
+       if (do_pythia_event_record) then
+          pythiafilename = trim(baseout)//'.txt'
+          open(pythiaLun, file=pythiafilename)
+          write(*,*) 'PYTHIA event record file: ', trim(pythiafilename)
+       end if
 
 c ---------------------------------------------------------------------
 C...Event generation loop
 c ---------------------------------------------------------------------
 
 C   This is what we write in the ascii-file
-       IF(.not.do_simc_out) THEN
-        write(29,*)' PYTHIA EVENT FILE '
-        write(29,*)'============================================'
-        write(29,30) 
+       if (do_pythia_event_record) then
+        write(pythiaLun,*)' PYTHIA EVENT FILE '
+        write(pythiaLun,*)'============================================'
+        write(pythiaLun,30)
 30      format('I, ievent, genevent, subprocess, nucleon,
      &  targetparton, xtargparton, beamparton, xbeamparton,
      &  thetabeamprtn, truey, trueQ2, truex, trueW2, trueNu, leptonphi, 
      &  s_hat, t_hat, u_hat, pt2_hat, Q2_hat, F2, F1, R, sigma_rad, 
      &  SigRadCor, EBrems, photonflux, t-diff, nrTracks')
-        write(29,*)'============================================'
+        write(pythiaLun,*)'============================================'
 
-        write(29,*)' I  K(I,1)  K(I,2)  K(I,3)  K(I,4)  K(I,5)
+        write(pythiaLun,*)' I  K(I,1)  K(I,2)  K(I,3)  K(I,4)  K(I,5)
      &  P(I,1)  P(I,2)  P(I,3)  P(I,4)  P(I,5)  V(I,1)  V(I,2)  V(I,3)'
-        write(29,*)'============================================'
-      ENDIF
+        write(pythiaLun,*)'============================================'
+      end if
         ! write(*,*) ''
         ! write(*,*) 'New Event:'
         ! write(*,*) 'mcRadCor_EBrems                       = ', mcRadCor_EBrems
@@ -356,9 +468,105 @@ C         write(*,*) radgamEnucl, radgamE, dplabg(3), radgamp
             nrtrack=tracknr
          endif
 
+C----- Optional event filter: require PDG to appear in event -----------
+         if (do_keep_pdg) then
+            has_keep_pdg = .false.
+            do I = 1, tracknr
+               if (keep_final_only) then
+                  if (K(I,1) .ne. 1) cycle
+               end if
+
+               if (keep_abs_pdg) then
+                  if (abs(K(I,2)) .eq. abs(keep_pdg)) then
+                     has_keep_pdg = .true.
+                     exit
+                  end if
+               else
+                  if (K(I,2) .eq. keep_pdg) then
+                     has_keep_pdg = .true.
+                     exit
+                  end if
+               end if
+            end do
+
+            if (.not.has_keep_pdg) cycle
+         end if
+
+C----- Exclusive selection: ep -> ep + KEEP_PDG (no other particles) ---
+         if (do_exclusive) then
+            if (.not. do_keep_pdg) then
+               write(*,*) 'ERROR: KEEP_EXCLUSIVE requires KEEP_PDG != 0'
+               stop
+            end if
+
+            if (.not. excl_allow_rad) then
+               if (RadState .ne. 0) cycle
+            end if
+
+            if (.not. is_exclusive_parent(keep_pdg, .false.)) cycle
+         end if
+
          if ((msti(1).ge.91).and.(msti(1).le.94)) msti(16)=0
-        if (.not.do_simc_out) then    
-         write(29,32) 0, ievent, genevent, msti(1), msti(12), 
+        if (do_simc_out) then
+!----- determine if SIMC would write this event ------------------------
+         simc_event_ok = .false.
+         foundE = .false.
+         hasPhi = .false.
+         do I = 1, tracknr
+            if (abs(K(I,2)) .eq. 333) then
+               hasPhi = .true.
+               exit
+            end if
+            if (.not.foundE .and. K(I,1) .eq. 1           
+     &        .and. abs(K(I,2)) .eq. 11                
+     &        .and. K(I,3) .eq. 3) then
+               foundE = .true.
+               pxE = P(I,1)
+               pyE = P(I,2)
+               pzE = P(I,3)
+               eE  = P(I,4)
+            end if
+         ENDDO
+         if (.not.hasPhi .and. foundE) then
+            phiE0 = atan2(pyE,pxE)
+            if (phiE0 .lt. 0d0) phiE0 = phiE0 + twoPi
+            sectorWidth = twoPi / 12.d0
+            deltaphi    = 1.57079632679d0                          
+     &              - (int(phiE0/sectorWidth) + 0.5d0)*sectorWidth
+            cosd = cos(deltaphi)
+            sind = sin(deltaphi)
+            if (do_pion) then
+               targetPID = 211
+            else if (do_kaon) then
+               targetPID = 321
+            else
+               targetPID = 2212
+            end if
+            deg2rad = pi2/180.d0
+            maxOff  = 20.d0*deg2rad
+
+            do I = 1, tracknr
+               if (K(I,1) /= 1 .or. K(I,2) /= targetPID) cycle
+               phiP0 = atan2(P(I,2),P(I,1))
+               delPhi = phiE0 - phiP0
+               if (delPhi > pi2) then
+                  delPhi = delPhi - twoPi
+               end if
+               if (delPhi < -pi2) then
+                  delPhi = delPhi + twoPi
+               end if
+               off = abs(abs(delPhi) - pi2)
+               if (off > maxOff) cycle
+               simc_event_ok = .true.
+               exit
+            end do
+         end if
+        end if
+        if (do_pythia_event_record) then    
+         if (do_simc_out .and. .not.simc_event_ok) then
+            goto 210
+         end if
+         write(pythiaLun,32) 0, ievent, genevent, msti(1), msti(12), 
      &        msti(16), pari(34), msti(15), pari(33), pari(53), 
      &        VINT(309), VINT(307), trueX, trueW2, trueNu,
      &        VINT(313), pari(14), pari(15), pari(16), 
@@ -368,11 +576,12 @@ C         write(*,*) radgamEnucl, radgamE, dplabg(3), radgamp
  32      format((I4,1x),(I10,1x),3(I4,1x),(I10,1x),f9.6,1x,
      &         I12,1x,
      &         2(f12.6,1x),7(f18.11,3x),12(f19.9,3x),I12)
-         write(29,*)'============================================'
+         write(pythiaLun,*)'===================='//
+     &        '========================'
 
          DO I=1,tracknr
          if (K(I,3).le.nrtrack) then
-         write(29,34) I,K(I,1),K(I,2),K(I,3),K(I,4),K(I,5),
+         write(pythiaLun,34) I,K(I,1),K(I,2),K(I,3),K(I,4),K(I,5),
      &        P(I,1),P(I,2),P(I,3),P(I,4),P(I,5),
      &        V(I,1),V(I,2),V(I,3)
          endif
@@ -409,7 +618,7 @@ C         write(*,*) radgamEnucl, radgamE, dplabg(3), radgamp
                 ! The beam or target PIDs changed before getting to radgen - cannot modify in case of ISR
                 ! Specifically for if the beam and target are not an electron and proton (other configurations not available yet)
                 ! If the beam's PID changed, but the event would be FSR, then the photon's status will still be 56 (57 is only used for if it should have been 55 if not for the different PID)
-            write(29,34) nrtrack, 54+RadState, 22, 1, 0, 0,
+            write(pythiaLun,34) nrtrack, 54+RadState, 22, 1, 0, 0,
      &      sngl(dplabg(1)),sngl(dplabg(2)),sngl(-radgamp),
      &      sngl(radgamE), 0., 0., 0., 0.
          else if (RadState.NE.0) then
@@ -424,8 +633,10 @@ C         write(*,*) radgamEnucl, radgamE, dplabg(3), radgamp
              write(*,*)""
          endif
  34      format(2(I6,1x),I10,1x,3(I8,1x),8(f15.6,1x))
-         write(29,*)'=============== Event finished ==============='
-      else
+         write(pythiaLun,*)'============ Event finished ============'
+      end if
+ 210  continue
+      if (do_simc_out) then
 !----- 1) veto φ and find the scattered e⁻ exactly as before ----------
          foundE = .false.
          hasPhi = .false.
@@ -502,7 +713,7 @@ C         write(*,*) radgamEnucl, radgamE, dplabg(3), radgamp
          call random_number(vxloc)
          vxloc = -7.5d0 + 15.d0 * vxloc
 
-         write(29,'(11F12.6)') 
+         write(simcLun,'(11F12.6)') 
      &      pxP,pyP,pzP,eP,vxloc,
      &      pxE,pyE,pzE,eE,vxloc,1.0
 
@@ -528,7 +739,12 @@ C   normalisation the number is in microbarns
        write(*,*)'Total Number of generated events', MSTI(5)
        write(*,*)'Total Number of trials', NGEN(0,3)
        write(*,*)'==================================================='
-       close(29)
+       if (do_simc_out) then
+          close(simcLun)
+       end if
+       if (do_pythia_event_record) then
+          close(pythiaLun)
+       end if
 
   500  if (qedrad.eq.2) then
          write(*,*) 'lookup table is generated;'
@@ -538,6 +754,115 @@ C   normalisation the number is in microbarns
 C...Check pdf status       
        call PDFSTA
        END
+
+      logical function is_exclusive_parent(parent_pdg, allow_photons)
+      include 'pythia-radcorr/pythia.inc'
+      integer parent_pdg
+      logical allow_photons
+
+      integer i, mom
+      integer nE, nP, nParent, nParentFinal
+      logical ok_from_parent
+      real*8 sum_px, sum_py, sum_pz, sum_e, mass2, inv_mass
+      real*8 excl_mass_tol
+      COMMON /EXCLCUTS/ excl_mass_tol
+
+      nE = 0
+      nP = 0
+      nParent = 0
+      nParentFinal = 0
+      sum_px = 0d0
+      sum_py = 0d0
+      sum_pz = 0d0
+      sum_e = 0d0
+
+C-- count parent occurrences anywhere in record (decayed parent may be status 2)
+      do i = 1, N
+         if (abs(K(i,2)) .eq. abs(parent_pdg)) then
+            nParent = nParent + 1
+            if (K(i,1) .eq. 1) nParentFinal = nParentFinal + 1
+         end if
+      end do
+
+      if (nParent .lt. 1) then
+         is_exclusive_parent = .false.
+         return
+      end if
+      if (nParentFinal .gt. 1) then
+         is_exclusive_parent = .false.
+         return
+      end if
+
+C-- loop over final-state particles only
+      do i = 1, N
+         if (K(i,1) .ne. 1) cycle
+
+C-- allow the scattered lepton and recoil proton
+         if (abs(K(i,2)) .eq. 11) then
+            nE = nE + 1
+            cycle
+         end if
+         if (K(i,2) .eq. 2212) then
+            nP = nP + 1
+            cycle
+         end if
+
+C-- optionally allow final-state photons (from PYJETS)
+         if (K(i,2) .eq. 22 .and. allow_photons) cycle
+
+C-- allow the parent itself if you kept it stable (parent as final-state)
+         if (abs(K(i,2)) .eq. abs(parent_pdg)) then
+            sum_px = sum_px + dble(P(i,1))
+            sum_py = sum_py + dble(P(i,2))
+            sum_pz = sum_pz + dble(P(i,3))
+            sum_e  = sum_e  + dble(P(i,4))
+            cycle
+         end if
+
+C-- otherwise: require this final particle to descend from the parent
+         ok_from_parent = .false.
+         mom = K(i,3)
+  10     continue
+         if (mom .le. 0) goto 20
+         if (abs(K(mom,2)) .eq. abs(parent_pdg)) then
+            ok_from_parent = .true.
+            goto 20
+         end if
+         mom = K(mom,3)
+         goto 10
+
+  20     continue
+         if (.not. ok_from_parent) then
+            is_exclusive_parent = .false.
+            return
+         end if
+         sum_px = sum_px + dble(P(i,1))
+         sum_py = sum_py + dble(P(i,2))
+         sum_pz = sum_pz + dble(P(i,3))
+         sum_e  = sum_e  + dble(P(i,4))
+      end do
+
+C-- require exactly one scattered lepton and one proton
+      if (nE .eq. 1 .and. nP .eq. 1) then
+         if (sum_e .le. 0d0) then
+            is_exclusive_parent = .false.
+            return
+         end if
+         mass2 = sum_e**2 - sum_px**2 - sum_py**2 - sum_pz**2
+         if (mass2 .lt. 0d0) mass2 = 0d0
+         inv_mass = sqrt(mass2)
+         if (abs(inv_mass - dble(PYMASS(parent_pdg))) .le.
+     &       excl_mass_tol) then
+            is_exclusive_parent = .true.
+         else
+            is_exclusive_parent = .false.
+         end if
+      else
+         is_exclusive_parent = .false.
+      end if
+
+      return
+      end
 
       ! ================================================================
       ! Return a kind=8 integer from the system clock.
